3663516d00ab515d499c641cd4605ebc933437c5
ARJUN RAGHUNANDANAN <42718953+ARJUNRAGHUNANDANAN@users.noreply.github.com>
2024-08-02 00:21:46 +0530

    Update streamlit_app.py
    - Adding Raw Data
    - Choosing Features & Label.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,21 @@
 import streamlit as st
-
+import pandas as pd
 st.title('🧮 Streamlit Machine Learning App')
 
 st.write('Hello world!')
 
 st.info('This is a Streamlit Cloud Python App that builds a ML Model.')
+
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  # Taken from Github -> dataprofessor -> data -> penguins_cleaned.csv
+  df = pdf.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+
+  st.write('**X**')
+  x = df.drop('species, axis=1')
+  x
+
+  st.write(**Y**)
+  y = df.species
+  y
