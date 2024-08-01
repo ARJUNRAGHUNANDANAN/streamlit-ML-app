cd06c557066522213448d35f68348684410a6b25
ARJUN RAGHUNANDANAN <42718953+ARJUNRAGHUNANDANAN@users.noreply.github.com>
2024-08-02 01:26:57 +0530

    Update streamlit_app.py
    - Add Sidebar.
    - Get user input data for prediction.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,39 @@ with st.expander('Data'):
 
 with st.expander('Visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+with st.sidebar:
+  st.header('Input Features')
+  island = st.selectbox('Island', ('Torgersen', 'Biscoe', 'Dream')) 
+  # x['island'].unique() gives 'Torgersen', 'Biscoe', 'Dream'
+  
+  gender = st.radio('gender', ('male','female'))
+  # x['sex'].unique()
+  
+  bill_length_mm = st.slider('Bill Length in mm', 32.1, 59.6, 43.9)
+  # x['bill_length_mm'].min(), x['bill_length_mm'].max(), x['bill_length_mm'].mean()
+
+  bill_depth_mm = st.slider('Bill Depth in mm', 13.1, 21.5, 17.16)
+  # x['bill_depth_mm'].min(), x['bill_depth_mm'].max(), x['bill_depth_mm'].mean()
+
+  flipper_length_mm = st.slider('Flipper Length in mm', 172,231, 200.96)
+  # x['flipper_length_mm'].min(), x['flipper_length_mm'].max(), x['flipper_length_mm'].mean()
+
+  body_mass_g = st.slider('Flipper Length in mm', 2700, 6300, 4201.05)
+  # x['body_mass_g'].min(), x['body_mass_g'].max(), x['body_mass_g'].mean()
+
+  data = {'island', island, 
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'gender': gender}
+
+  input_df = pf.DataFrame(data, index=[0])
+  input_penquins = pd.concat([input_df, X] axis=0)
+                             
+with st.expander('Input Features'): 
+  st.write('**Input Penquins')
+  input_df
+  st.write('**Combined Data**')
+  input_penquins
